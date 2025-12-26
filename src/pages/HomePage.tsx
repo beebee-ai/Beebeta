4cfb373056734f600582ef833aeef48b2134891a
chensihai230 <csh2302137484@gmail.com>
2025-12-26 17:31:48 +0800

    beta版本修改

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import { Header } from '../components/Header';
 import { HeroSection } from '../components/HeroSection';
 import { FeaturesSection } from '../components/FeaturesSection';
 import { CourseSection } from '../components/CourseSection';
-import { AdvancedCoursesAndStatsSection } from '../components/AdvancedCoursesAndStatsSection';
+import { AdvancedCoursesAndStatsSection } from '../components/AdvancedCoursesAndStatsSection.tsx';
 import { WorksSection } from '../components/WorksSection';
 import { TeamSection } from '../components/TeamSection';
 import { AboutSection } from '../components/AboutSection';
